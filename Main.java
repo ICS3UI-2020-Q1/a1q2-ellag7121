276bfcdfc938acc8f8be75e01fc00d6672de43e6
ellag7121 <ellag7121@wrdsb.ca>
2020-09-14 14:00:58 +0000

    Finished A1Q2!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * get user input for two numbers, divide the two numbers, tell the user the quotient and the remainder
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner used to get input
+    Scanner input = new Scanner(System.in);
     
+    //initialize variables
+    int firstNum; //variable to store first number
+    int secondNum; //variable to store second number
+    int quotient; //the quotient of the numbers
+    int remainder; //the remainder of the two divided numbers
+
+    //ask for user input
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    //get numbers from user
+    firstNum = input.nextInt(); //get first number to divide
+    secondNum = input.nextInt(); //get second number to divide
+
+    //divide the numbers and store in the quotient variable
+    quotient = firstNum/secondNum;
+    //get the remainder of the numbers and store it in the remainder variable
+    remainder = firstNum%secondNum;
+
+    //tell the user the result
+    System.out.println(firstNum + "/" + secondNum + " is " + quotient + " with a remainder of " + remainder + ".");
+
+
   }
 }
